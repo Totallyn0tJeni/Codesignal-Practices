a1e144f2cf37b2c93015e66b62b05cad1dcaf708
Jenisha Patel <jeni.1245690@gmail.com>
2024-10-20 13:10:26 -0400

    Create Travel Logic.py

---
+++ Travel Logic.py
@@ -0,0 +1,16 @@
+# TODO: Declare variables 'age' and 'budget' with appropriate values
+age = 20  # Example age
+budget = 1200  # Example budget
+
+# TODO: Write an 'if' statement to determine if the traveler is over 18
+if age > 18:
+    # TODO: Inside the 'if' block, write another 'if' statement to check if the budget is more than 1000
+    if budget > 1000:
+        # TODO: Print "You are eligible for the international travel package." for budgets over 1000
+        print("You are eligible for the international travel package.")
+    # TODO: Else block for budgets 1000 or less
+    else:
+        print("You are eligible for the local travel package.")
+# TODO: Else block for travelers 18 or below
+else:
+    print("You are eligible for the children's travel package.")
