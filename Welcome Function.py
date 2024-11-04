51c8c53c755f4e7e2c6db43a717cd359b17a88ea
Jenisha Patel <jeni.1245690@gmail.com>
2024-11-04 15:42:35 -0500

    Create Welcome Function.py

---
+++ Welcome Function.py
@@ -0,0 +1,6 @@
+# TODO: Write a function named greet_with_welcome that takes two parameters: name and hometown.
+def greet_with_welcome(name, hometown):
+    print(f"Welcome {name}, to {hometown}.")
+# The function should print a greeting to the user that includes their name and hometown.
+
+greet_with_welcome("Jenisha", "Seoul")
